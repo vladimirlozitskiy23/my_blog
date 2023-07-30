07076a7e8b6e78a46fd325e9c711208b617ed19b
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-07-30 21:14:46 +0300

    Add devise gem

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ end
 gem "jsbundling-rails", "~> 1.1"
 
 gem "cssbundling-rails", "~> 1.2"
+
+gem "devise", "~> 4.9"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  devise_for :users
+  # root to: "home#index"
+  root to: "articles#index"
   resources :articles, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
