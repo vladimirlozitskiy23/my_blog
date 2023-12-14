cfe6c8832c311d4fa673ae239141288f092f1555
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-12-14 19:00:31 +0300

    Add reverse article lits

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
 
     # before_action :authenticate_user!
-
+    def before_action
+      @posts = Article.all
+    end
     def index
         #  @articles = Article.all 
         @q = Article.ransack(params[:q])
         @articles = @q.result
+        @posts = Article.all
     end
 
     def show 
